e1be48222a891fce61fff709121c56aa81321e86
Jean Andreotti Borges da Silva <jeandreotti25@gmail.com>
2024-10-04 17:46:00 -0300

    acrescentado css
    acrescentado css

--- javascript.js
+++ javascript.js
@@ -2,7 +2,9 @@
     function calcular(){
     
         var peso1 = window.document.getElementById('peso').value;
+
         var valor1 = window.document.getElementById('valor').value;
+        
         var proteina1 = window.document.getElementById('proteina').value;
     
         var valorPeso = Number((0.1/ peso1) * valor1);
