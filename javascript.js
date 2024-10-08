2bcd350e0f6f7ee78b6d898a4a0f2b760b7c9e9b
Jean Andreotti Borges da Silva <jeandreotti25@gmail.com>
2024-10-08 18:44:34 -0300

    ...
    ...

--- javascript.js
+++ javascript.js
@@ -9,7 +9,12 @@
     
         var valorPeso = Number((0.1/ peso1) * valor1);
         var valorproteina = Number(valorPeso/proteina1);
+
+        var prot100 = window.document.getElementById('mostrarProt');
     
         document.getElementById('totalVP').innerHTML += valorPeso;
         document.getElementById('totalProt').innerHTML += valorproteina;
+
+        prot100.innerHTML = `${proteina1}gr`;
+        
         }
