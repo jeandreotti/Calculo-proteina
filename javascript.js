ae9741273ef29d3cd1db27b81ee6c96919311389
Jean Andreotti Borges da Silva <jeandreotti25@gmail.com>
2024-12-17 06:41:37 -0300

    otimizado casas, virgulas

--- javascript.js
+++ javascript.js
@@ -1,20 +1,30 @@
 
     function calcular(){
     
-        var peso1 = window.document.getElementById('peso').value;
+        let peso = window.document.getElementById('peso').value;
 
-        var valor1 = window.document.getElementById('valor').value;
+        let valor = window.document.getElementById('valor').value;
         
-        var proteina1 = window.document.getElementById('proteina').value;
+        let proteina = window.document.getElementById('proteina').value;
     
-        var valorPeso = Number((0.1/ peso1) * valor1);
-        var valorproteina = Number(valorPeso/proteina1);
+        let peso1 = parseFloat(peso.replace(',' , '.'));
+        let valor1 = parseFloat(valor.replace(',' , '.'));
+        let proteina1 = parseFloat(proteina.replace(',' , '.'));
+       
+        
+        let valorPeso = ((0.1/ peso1) * valor1);
+        let valorProteina = (valorPeso/proteina1);
 
-        var prot100 = window.document.getElementById('mostrarProt');
-    
-        document.getElementById('totalVP').innerHTML += valorPeso;
-        document.getElementById('totalProt').innerHTML += valorproteina;
+        valorPeso = valorPeso.toFixed(2).replace("." , ",");
+        valorProteina = valorProteina.toFixed(2).replace("." , ",");;
 
+        document.getElementById('totalVP').innerHTML = 'R$ ' + valorPeso;
+        document.getElementById('totalProt').innerHTML = 'R$ ' + valorProteina;
+
+        let prot100 = window.document.getElementById('mostrarProt');
+
+        prot100.style.color = 'black';
+        prot100.style.fontSize = '1.4em';
         prot100.innerHTML = `${proteina1}gr`;
         
         }
